72ffaf495a8ce2c5539c964e59de6077d5431a6e
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-03 23:22:00 +0530

    middleware code

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,33 @@
+
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req,res, next) {
+  const authentication = req.headers.Authorization;
+
+  if(!authentication) {
+    return res.status(401).json({
+      message: "No token provided"
+    });
+  }
+
+  const token = authentication.split(' ')[1];
+
+  try {
+    const decoded  = jwt.verify(token, process.env.SECRET_KEY);
+
+    if(!decoded.id) {
+      req.id = decoded.id;
+      next();
+    } else {
+      return res.status(411).json({
+        message: "Invalid token",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized"
+    });
+  }
+}
+
+module.exports = authMiddleware;
