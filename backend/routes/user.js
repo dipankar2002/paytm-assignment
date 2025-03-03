aae7b4158c3fefc31856767d83a2031f58a31183
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-03 23:22:33 +0530

    update and find user code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,90 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const userSchema = require('../zod/user');
 const User = require('../db');
+const authMiddleware = require('../middleware/middleware');
 const router = express.Router();
+
+router.put('/', authMiddleware ,async (req, res) => {
+  const { password, firstName, lastName } = req.body;
+  const id = req.id;
+
+  const userValidation = userSchema.safeParse({ password, firstName, lastName });
+  if(!userValidation.success) {
+    return res.status(411).json({
+      message: "Error while updating information"
+    });
+  }
+
+  const oldUser = User.findOne({_id: id});
+
+  const updateUser = await User.findOneAndUpdate({_id: id}, {
+    password: password? password: oldUser.password,
+    firstName: firstName? firstName: oldUser.firstName,
+    lastName: lastName? lastName: oldUser.lastName
+  })
+  console.log(updateUser);
+  res.status(200).json({
+    message: "User information updated successfully"
+  });
+});
+
+// async function findKey(value) {
+//   const user = await User.findOne();
+//   if(!user) {
+//     return null;
+//   }
+//   for (const key in user.toObject()) {
+//     if (user[key] === value) {
+//       console.log(`Found! Key: ${key}, Value: ${value}`);
+//       return key;
+//     }
+//   }
+//   return null;
+// }
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+  
+  if(filter) {
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+    return res.status(200).json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+  }
+  const user = await User.find();
+  res.status(200).json(user);
+})
+
+// ------------ This code by me -----------
+// router.get('/bulk', authMiddleware, async (req, res) => {
+//   const filter = req.query.filter;
+//   const id = req.id;
+
+//   const key = findKey(filter);
+
+//   if(filter && key) {
+//     const filterUser = await User.findOne({key: filter});
+//     return res.status(200).json(filterUser);
+//   }
+
+//   const user = await User.findOne({_id: id});
+//   res.status(200).json(user);
+// });
 
 router.post('/signin', async (req, res) => {
   const {username, password} = req.body;
