93aa1244284d5eb8ad1f5244f9a060278c61a9f0
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-04 14:27:01 +0530

    add account schema

--- backend/db.js
+++ backend/db.js
@@ -5,22 +5,46 @@ mongoose.connect(process.env.MONGODB_URL);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    trim: true,
+    unique: true,
+    lowercase: true,
     required: true
   },
   password: {
     type: String,
+    minlength: 6,
     required: true
   },
   firstName: {
     type: String,
+    trim: true,
+    maxlength: 50,
     required: true
   },
   lastName: {
     type: String,
+    trim: true,
+    maxlength: 50,
     required: true
   },
-})
+});
 
+const acountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+});
+
+const Account = mongoose.model('Account', acountSchema);
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = {
+  User,
+  Account
+};
