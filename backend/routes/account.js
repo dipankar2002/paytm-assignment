65b54997157c6e8d74a5f6c2b226e51e8779e6d0
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-05 11:23:25 +0530

    best way for transaction using mongoose session

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,38 +1,77 @@
 const express = require('express');
 const authMiddleware = require('../middleware/middleware');
 const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
 
 router.post('/transfer', authMiddleware ,async (req, res) => {
+  const session = await mongoose.startSession();
+
+  session.startTransaction();
   const { amount, to } = req.body;
   const id = req.id;
-  const senderAccount = await Account.findOne({userId: id});
-  if(!senderAccount) {
-    return res.status(411).json({
-      message: "Sender account not found",
+
+  // Fetch the accounts within the transaction
+  const account = await Account.findOne({ userId: id }).session(session);
+
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance"
     });
   }
-  if(senderAccount.balance < amount) {
-    return res.status(411).json({
-      message: "Insufficient balance",
+
+  const toAccount = await Account.findOne({ userId: to }).session(session);
+
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid account"
     });
   }
-  const receiverAccount = await Account.findOne({userId: to});
-  if(!receiverAccount) {
-    return res.status(411).json({
-      message: "Receiver account not found",
-    });
-  }
-  await Account.updateOne({userId:id},{
-    balance: senderAccount.balance - amount
-  })
-  await Account.updateOne({userId:to},{
-    balance: receiverAccount.balance + amount
-  })
-  res.status(200).json({
+
+  // Perform the transfer
+  await Account.updateOne({ userId: id }, { $inc: { balance: -amount } }).session(session);
+  await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+  // Commit the transaction
+  await session.commitTransaction();
+  res.json({
     message: "Transfer successful"
   });
 });
+
+// ------------ This code by me \\ Which is bad way transactions -----------
+// router.post('/transfer', authMiddleware ,async (req, res) => {
+//   const { amount, to } = req.body;
+//   const id = req.id;
+//   const senderAccount = await Account.findOne({userId: id});
+//   if(!senderAccount) {
+//     return res.status(411).json({
+//       message: "Sender account not found",
+//     });
+//   }
+//   if(senderAccount.balance < amount) {
+//     return res.status(411).json({
+//       message: "Insufficient balance",
+//     });
+//   }
+//   const receiverAccount = await Account.findOne({userId: to});
+//   if(!receiverAccount) {
+//     return res.status(411).json({
+//       message: "Receiver account not found",
+//     });
+//   }
+//   await Account.updateOne({userId:id},{
+//     balance: senderAccount.balance - amount
+//   })
+//   await Account.updateOne({userId:to},{
+//     balance: receiverAccount.balance + amount
+//   })
+//   res.status(200).json({
+//     message: "Transfer successful"
+//   });
+// });
 
 router.get('/balance', authMiddleware ,async (req, res) => {
   const id = req.id;
