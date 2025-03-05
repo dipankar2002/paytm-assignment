9ddeddfbb20e113fc439846e8a8f148fadb3c9b4
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-05 22:47:12 +0530

    create all the compo files and routes them in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import SendMoney from "./components/SendMoney";
+import DashBoard from "./components/DashBoard";
+
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" elements={<SignUp/>} />
+        <Route path="/signin" elements={<SignIn />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
