7ec468a6dfbb7295ac33837bbdae57cc3f8fae75
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-09 15:38:26 +0530

    change in route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp/>} />
+        <Route path="/signup" element={<SignUp/>} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/" element={<DashBoard />} />
         <Route path="/send" element={<SendMoney />} />
       </Routes>
     </BrowserRouter>
